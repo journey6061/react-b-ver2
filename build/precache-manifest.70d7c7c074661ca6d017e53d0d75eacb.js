adc2cfdc58fc124651b2ba2ae0773375f801f186
Alex Zhang <>
2020-01-10 17:31:05 -0500

    file init

---
+++ build/precache-manifest.70d7c7c074661ca6d017e53d0d75eacb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe022d35931b994b6263034498f7e36d",
+    "url": "/react-b/index.html"
+  },
+  {
+    "revision": "f50f5547cbdc8292bc37",
+    "url": "/react-b/static/css/main.9f029f7b.chunk.css"
+  },
+  {
+    "revision": "d742d450b6dd5484aed4",
+    "url": "/react-b/static/js/2.3c8296cd.chunk.js"
+  },
+  {
+    "revision": "f50f5547cbdc8292bc37",
+    "url": "/react-b/static/js/main.27b233f5.chunk.js"
+  },
+  {
+    "revision": "330ae53b1b20aec8f340",
+    "url": "/react-b/static/js/runtime-main.74f880dd.js"
+  }
+]);
